af37e9c26999147737421dae934fd6785ba71d56
Eric Defore <eric.defore@gmail.com>
2017-04-10 14:46:58 -0400

    Conditionally show Checkbox on Checkout form

--- core/ssl-only/slack-invites/class-edd-slack-app-invites.php
+++ core/ssl-only/slack-invites/class-edd-slack-app-invites.php
@@ -58,18 +58,36 @@ class EDD_Slack_Invites {
 		
 		$label = edd_get_option( 'slack_app_join_team_team_text', apply_filters( 'edd_slack_app_join_slack_team_default_text', _x( 'Join our Slack Team?', 'Join Slack Team Text Default', 'edd-slack' ) ) );
 		
-		?>
+		$show_checkbox = false;
 		
-		<fieldset id="edd_slack_send_customer_team_invite_fieldset">
-			<div class="edd-slack-send-customer-team-invite">
-				<input name="edd_slack_send_customer_team_invite" type="checkbox" id="edd_slack_send_customer_team_invite" value="1" <?php checked( $checked, 1, true ); ?>/>
-				<label for="edd_slack_send_customer_team_invite">
-					<?php echo apply_filters( 'edd_slack_app_join_slack_team_default_text', _x( 'Join our Slack Team?', 'Join Slack Team Text Default', 'edd-slack' ) ); ?>
-				</label>
-			</div>
-		</fieldset>
+		// Determine whether we should show the Checkbox
+		// For non-logged in Customers, we will always need to show it as we have no way to know if they have been invited until they complete the form
+		// For logged in Customers, we can know before hand
+		if ( ! is_user_logged_in() ) {
+			$show_checkbox = true;
+		}
+		else {
+			
+			$customer = new EDD_Customer( get_current_user_id() );
+			
+			if ( ! $customer->get_meta( 'edd_slack_app_invite_sent', true ) ) {
+				$show_checkbox = true;
+			}
+			
+		}
+		
+		if ( $show_checkbox ) : ?>
+		
+			<fieldset id="edd_slack_send_customer_team_invite_fieldset">
+				<div class="edd-slack-send-customer-team-invite">
+					<input name="edd_slack_send_customer_team_invite" type="checkbox" id="edd_slack_send_customer_team_invite" value="1" <?php checked( $checked, 1, true ); ?>/>
+					<label for="edd_slack_send_customer_team_invite">
+						<?php echo apply_filters( 'edd_slack_app_join_slack_team_default_text', _x( 'Join our Slack Team?', 'Join Slack Team Text Default', 'edd-slack' ) ); ?>
+					</label>
+				</div>
+			</fieldset>
 
-	<?php
+		<?php endif;
 		
 	}
 	
@@ -83,17 +101,30 @@ class EDD_Slack_Invites {
 	 * @return		void
 	 */
 	public function add_customer_to_slack_team_via_form( $payment_id ) {
+			
+		$customer_id = get_post_meta( $payment_id, '_edd_payment_customer_id', true );
+		$customer = new EDD_Customer( $customer_id );
 		
 		// If they've Opted-in to being added to the Slack Team
 		if ( ! empty( $_POST['edd_slack_send_customer_team_invite'] ) ) {
-		
-			$email = $_POST['edd_email'];
-			$first_name = $_POST['edd_first'];
-			$last_name = $_POST['edd_last'];
-
-			$channels = implode( ',', edd_get_option( 'slack_app_team_invites_customer_channels', array() ) );
 			
-			$this->send_invite( $email, $channels, $first_name, $last_name );
+			// If an invite has not been sent for this customer
+			// This is an important distinction since we cannot know "who" a Customer is until they've filled out the Form
+			// If they're logged in as a USER, then we can know. But Customers don't necessarily need to be logged in
+			if ( ! $customer->get_meta( 'edd_slack_app_invite_sent', true ) ) {
+		
+				$email = $_POST['edd_email'];
+				$first_name = $_POST['edd_first'];
+				$last_name = $_POST['edd_last'];
+
+				$channels = implode( ',', edd_get_option( 'slack_app_team_invites_customer_channels', array() ) );
+				
+				$this->send_invite( $email, $channels, $first_name, $last_name );
+			
+				// Record that we've sent them an invite
+				$customer->add_meta( 'edd_slack_app_invite_sent', true, true );
+				
+			}
 			
 		}
 		

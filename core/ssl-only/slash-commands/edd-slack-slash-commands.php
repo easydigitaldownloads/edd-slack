7a7374e3d29492e1600966ac30b4f2f123bb7ea0
Eric Defore <eric.defore@gmail.com>
2017-08-10 16:26:07 -0400

    /edd discount Referencing #56

--- core/ssl-only/slash-commands/edd-slack-slash-commands.php
+++ core/ssl-only/slash-commands/edd-slack-slash-commands.php
@@ -52,6 +52,14 @@ if ( ! function_exists( 'edd_slack_slash_command_help' ) ) {
 			'version' => array(
 				'description' => _x( 'Outputs the current version of Easy Digital Downloads.', '/edd version Description', 'edd-slack' ),
 			),
+			'discount' => array(
+				'description' => _x( 'Outputs information about a Discount Code. This can also be used to create new Discount Codes.', '/edd discount Description', 'edd-slack' ),
+				'examples' => array(
+					_x( 'DISCOUNTCODE', '/edd discount Default Example', 'edd-slack' ),
+					sprintf( _x( 'DISCOUNTCODE %s', '/edd discount Dollar Amount Example', 'edd-slack' ), html_entity_decode( edd_currency_filter( '3.50' ) ) ),
+					sprintf( _x( 'DISCOUNTCODE 42%%', '/edd discount Percentage Example', 'edd-slack' ) ),
+				),
+			),
 		) );
 		
 		$commands['help'] = array(
@@ -84,16 +92,33 @@ if ( ! function_exists( 'edd_slack_slash_command_help' ) ) {
 				$description = '';
 			}
 			
-			$example = "\n" . sprintf( _x( 'Example: `%s', 'Slash Command Example', 'edd-slack' ), $request_body['command'] . ' ' . $command );
-			
-			// If there's a default set, make some strings
-			if ( isset( $args['default'] ) && ! empty( $args['default'] ) ) {
-				$example .= ' ' . $args['default'] . '`';
-				$default = "\n" . sprintf( _x( 'Default: `%s`', 'Slash Command Default', 'edd-slack' ), $args['default'] );
+			// Allow Examples to be explicitly provided for more complex commands
+			if ( isset( $args['examples'] ) && 
+			   ! empty ( $args['examples'] ) ) {
+				
+				$example = "\n" . _x( 'Examples:', 'Slash Command Multiple Examples', 'edd-slack' );
+				
+				foreach ( $args['examples'] as $example_text ) {
+					
+					$example .= "\n\t`" . $request_body['command'] . ' ' . $command . ' ' . $example_text . '`';
+					
+				}
+				
 			}
 			else {
-				$example .= '`';
-				$default = '';
+			
+				$example = "\n" . sprintf( _x( 'Example: `%s', 'Slash Command Example', 'edd-slack' ), $request_body['command'] . ' ' . $command );
+
+				// If there's a default set, make some strings
+				if ( isset( $args['default'] ) && ! empty( $args['default'] ) ) {
+					$example .= ' ' . $args['default'] . '`';
+					$default = "\n" . sprintf( _x( 'Default: `%s`', 'Slash Command Default', 'edd-slack' ), $args['default'] );
+				}
+				else {
+					$example .= '`';
+					$default = '';
+				}
+				
 			}
 			
 			if ( isset( $args['options'] ) && ! empty( $args['options'] ) ) {
@@ -230,6 +255,125 @@ if ( ! function_exists( 'edd_slack_slash_command_version' ) ) {
 				'icon' => function_exists( 'has_site_icon' ) && has_site_icon() ? get_site_icon_url( 270 ) : '',
 				'attachments' => $attachments,
 			)
+		);
+		
+	}
+	
+}
+
+if ( ! function_exists( 'edd_slack_slash_command_discount' ) ) {
+
+	/**
+	 * Return information about a discount code
+	 * Alternatively, create one if it does not exist
+	 * 
+	 * @param	  string $values	   If this String is non-empty, we're changing the Version (Unused)
+	 * @param	  string $response_url Webhook to send the Response Message to
+	 * @param	  array  $request_body POST'd data from the Slack Client
+	 *															  
+	 * @since	  1.1.0
+	 * @return	  void
+	 */
+	function edd_slack_slash_command_discount( $values, $response_url, $request_body ) {
+		
+		$values = explode( ' ', trim( $values ) );
+		
+		// Alphanumeric Only
+		$discount_code = trim( $values[0] );
+		$discount_code = preg_replace( '/([^A-Z0-9])+/i', '', $discount_code );
+		
+		// Determine whether we're creating a Discount Code or not
+		$amount = ( isset( $values[1] ) && ! empty ( $values[1] ) ) ? trim( $values[1] ) : false;
+		
+		$response_text = '';
+		
+		// If amount was passed, then we are creating a Discount
+		if ( $amount ) {
+			
+			$discount = new EDD_Discount( $discount_post_id, false, false );
+			
+			$discount->__set( 'name', $discount_code );
+			$discount->__set( 'code', $discount_code );
+			$discount->__set( 'status', 'active' );
+			
+			if ( strpos( $amount, '%' ) !== false ) {
+				$discount->__set( 'type', 'percent' );
+			}
+			else {
+				$discount->__set( 'type', 'flat' );
+			}
+			
+			$discount->__set( 'amount', $amount );
+			
+			$discount->save();
+			
+			$response_title = sprintf( _x( 'Discount Code Created: %s', 'Title for /edd discount Creation', 'edd-slack' ), $discount_code );
+			
+		}
+		else {
+			
+			$discount = new EDD_Discount( $discount_code, true );
+			$response_title = sprintf( _x( 'Discount Code: %s', 'Title for /edd discount Data', 'edd-slack' ), $discount->get_code() );
+			
+		}
+		
+		$success = false;
+		
+		// Discount exists, either previously or was just created
+		if ( $discount->get_ID() !== 0 ) {
+			
+			$success = true;
+		
+			$date_format = get_option( 'date_format', 'F j, Y' );
+			$time_format = get_option( 'time_format', 'g:i a' );
+
+			$discount_amount = ( $discount->get_type() == 'flat' ) ? html_entity_decode( edd_currency_filter( number_format( $discount->get_amount(), 2 ) ) ) : $discount->get_amount() . '%';
+			$discount_start = ( $discount->get_start() ) ? date_i18n( $date_format . ' ' . $time_format, strtotime( $discount->get_start() ) ) : _x( 'No Start Date', 'Discount No Start Date', 'edd-slack' );
+			$discount_expiration = ( $discount->get_expiration() ) ? date_i18n( $date_format . ' ' . $time_format, strtotime( $discount->get_expiration() ) ) : _x( 'No Expiration Date', 'Discount No Expiration Date', 'edd-slack' );
+			$discount_status = ( $discount->get_status() == 'active' ) ? __( 'Active', 'edd-slack' ) : __( 'Inactive', 'edd-slack' );
+
+			$response_text .= _x( 'Name: ', 'Discount Name', 'edd-slack' ) . $discount->get_name();
+			$response_text .= "\n" . _x( 'Amount: ', 'Discount Amount', 'edd-slack' ) . $discount_amount;
+			$response_text .= "\n" . _x( 'Uses: ', 'Discount Uses', 'edd-slack' ) . $discount->get_uses();
+			$response_text .= "\n" . _x( 'Start Date: ', 'Discount Start Date', 'edd-slack' ) . $discount_start;
+			$response_text .= "\n" . _x( 'Expiration: ', 'Discount Expiration', 'edd-slack' ) . $discount_expiration;
+			$response_text .= "\n" . _x( 'Status: ', 'Discount Status', 'edd-slack' ) . $discount_status;
+			
+		}
+		else {
+			
+			$response_text = '*' . sprintf( _x( 'Discount Code %s Not Found', 'Title for /edd discount Creation Failure', 'edd-slack' ), $discount_code ) . '*';
+			
+			$response_text .= "\n" . sprintf( _x( "Were you trying to create a Discount Code? You need to include an Amount to do so. Run %s for examples.", 'Were you trying to create a Discount Code helper text', 'edd-slack' ), '`' . $request_body['command'] . ' help discount' . '`' );
+			
+		}
+		
+		$notification_args = array(
+			'username' => get_bloginfo( 'name' ),
+			'icon' => function_exists( 'has_site_icon' ) && has_site_icon() ? get_site_icon_url( 270 ) : '',
+		);
+		
+		// On Failure we want to use `` code designators, but that does not work with Attachments
+		if ( $success ) {
+			
+			$attachments = array(
+				array(
+					'title' => $response_title,
+					'text' => $response_text,
+				),
+			);
+			
+			$notification_args['attachments'] = $attachments;
+			
+		}
+		else {
+			$notification_args['text'] = $response_text;
+		}
+		
+		// Response URLs are Incoming Webhooks
+		$response_message = EDDSLACK()->slack_api->push_incoming_webhook(
+			$response_url,
+			$notification_args
 		);
 		
 	}

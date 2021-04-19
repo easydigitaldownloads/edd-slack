d4c343b0d323deab4fc563bb31072ab55cf69dcd
Robin Cornett <hello@robincornett.com>
2021-04-19 13:51:16 -0400

    Fix edd_slack_get_users errors, constant pinging
    #129

--- core/edd-slack-functions.php
+++ core/edd-slack-functions.php
@@ -24,20 +24,35 @@ function EDDSLACK() {
 
 /**
  * Returns all Users from the Slack API
- * 
- * @since		1.1.0
- * @return		array Slack Users
+ *
+ * @since   1.1.0
+ * @return  array Slack Users
  */
 function edd_slack_get_users() {
-	
+
+	if ( wp_doing_ajax() ) {
+		return array();
+	}
+	$transient = get_transient( 'edd_slack_users' );
+	if ( $transient ) {
+		return $transient;
+	}
+
 	$oauth_token = edd_get_option( 'slack_app_oauth_token', false );
 
 	// Don't bother if we don't have an OAUTH Token
-	if ( ! $oauth_token || $oauth_token == '-1' ) return array();
+	if ( ! $oauth_token || '-1' == $oauth_token ) {
+		return array();
+	}
 
-	if ( ! $users_array = maybe_unserialize( get_transient( 'edd_slack_users' ) ) ) {
+	$users_array = maybe_unserialize( $transient );
+	if ( ! $users_array ) {
 
 		$result = EDDSLACK()->slack_api->get( 'users.list' );
+
+		if ( empty( $result->members ) ) {
+			return array();
+		}
 
 		$users = $result->members;
 
@@ -45,17 +60,20 @@ function edd_slack_get_users() {
 		foreach ( $users as $user ) {
 
 			// Let's not bother with Deleted Users
-			if ( $user->deleted ) continue;
+			if ( $user->deleted ) {
+				continue;
+			}
 
 			// No need for Slackbot to be in the list
-			if ( $user->id == 'USLACKBOT' ) continue;
+			if ( $user->id == 'USLACKBOT' ) {
+				continue;
+			}
 
 			$users_array[ $user->id ] = '@' . $user->name . ' (' . $user->real_name . ')';
 
 			if ( $user->is_admin ) {
 				$users_array[ $user->id ] .= ' - ' . __( 'Admin', 'edd-slack' );
 			}
-
 		}
 
 		set_transient( 'edd_slack_users', $users_array, DAY_IN_SECONDS );
@@ -63,34 +81,33 @@ function edd_slack_get_users() {
 	}
 
 	return $users_array;
-
 }
 
 /**
  * Check to see if a Slack User ID is a Slack Team Admin
- * 
+ *
  * @param		string  $slack_user_id Slack User ID
- *                                           
+ *
  * @since		1.1.0
  * @return		boolean True if Admin
  */
 function edd_slack_is_slack_admin( $slack_user_id = '0' ) {
-	
+
 	$oauth_token = edd_get_option( 'slack_app_oauth_token', false );
-	
+
 	if ( ! $oauth_token || $oauth_token == '-1' ) return array();
-	
+
 	$users_info_url = add_query_arg(
 		array(
 			'user' => $slack_user_id,
 		),
 		'users.info'
 	);
-	
+
 	$result = EDDSLACK()->slack_api->get( $users_info_url );
-	
+
 	if ( ! $result->ok ) return false;
-	
+
 	return $result->user->is_admin;
-	
+
 }
